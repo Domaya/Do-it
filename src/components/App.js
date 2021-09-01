a43735c2841544c97758cbda0a9d2e7a820b69d8
Domaya <c2hila@gmail.com>
2021-09-01 16:20:00 +0900

    To do 입력 받기

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import Home from "./Home"
 function App() {
   return (
     <div>
-      <p>Hello, world!</p>
+      <h2>JUST DO IT</h2>
       <Home></Home>
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
   const [list, setList] = useState("");
+  const [todo, setTodo] =  useState([])
 
+  const onListChange = (event) => {
+    const { target:{value} } = event;
+    setList(value);
+  }
+  const onSubmit = (event) => {
+    event.preventDefault();
+    // setTodo(todo.concat(list));
+    setTodo([...todo, list])
+    setList("");
+    console.log(todo);
+  }
+
+  
+  
   return(
-    <form>
-      <input value={list} type="text"></input>
+    <>
+    <form  onSubmit={onSubmit}>
+      <input name="things" onChange={onListChange} placeholder="To do" value={list} type="text"></input>
+      <input type="submit" value="등록"></input>
     </form>
+    <div>
+      <h5>{todo}</h5>
+    </div>
+    </>
   )
 }
 export default Home;
